e974e9ad9c1b990309fc8b24e0eec25da372f78a
Nicolás Bértolo <nicolasbertolo@gmail.com>
2013-10-30 20:34:00 -0300

    Agregado util::mm_p_usec

--- util_tiempo.h
+++ util_tiempo.h
@@ -4,4 +4,6 @@ namespace util
 {
 typedef unsigned long millis_t;
 typedef unsigned long micros_t;
+
+const float mm_p_usec(340.0 / 1E6 * 1E3); //340m/s (vel. sonido) / 1E6 (convertido a m/usec) * 1E3 (conv. a mm/usec)
 }
